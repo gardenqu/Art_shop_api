a2142a7b3e5035a330db441adb9593cc7b36ebf2
QGarden <qjgarden1@gmail.com>
2022-04-18 22:03:22 -0400

    host

--- wsgi.py
+++ wsgi.py
@@ -3,9 +3,7 @@ from src.flaskdr import create_app
 
 
 
-
-
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='ec2-3-228-135-242.compute-1.amazonaws.com', port=5002)
+    app.run(host='0.0.0.0', port=5002)
